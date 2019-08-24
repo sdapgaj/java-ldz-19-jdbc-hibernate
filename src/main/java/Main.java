e439e459a02fe409d25811cacf4c2b0194864ded
Piotr Gajowniczek <piotrgajow@gmail.com>
2019-08-24 12:10:20 +0200

    Add method which inserts loc into the DB

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
 
@@ -10,23 +11,45 @@ public class Main {
         String url = "jdbc:mysql://localhost:3306/javaldz19?serverTimezone=Europe/Warsaw";
         String user = "root";
         String password = "";
-        try(Connection connection = DriverManager.getConnection(url, user, password)) {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM locations");
-
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String city = resultSet.getString("city");
-                String address = resultSet.getString("address");
-                String name = resultSet.getString("name");
-                System.out.println(id + " - " + name + " - " + city + " - " + address);
-            }
-
-            resultSet.close();
-            statement.close();
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            insertDataAboutLocation(connection);
+            showLocationList(connection);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void showLocationList(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM locations");
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String city = resultSet.getString("city");
+            String address = resultSet.getString("address");
+            String name = resultSet.getString("name");
+            System.out.println(id + " - " + name + " - " + city + " - " + address);
+        }
+
+        resultSet.close();
+        statement.close();
+    }
+
+    private static void insertDataAboutLocation(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please fill in the city");
+        String city = scanner.nextLine();
+        System.out.println("Whats the address?");
+        String address = scanner.nextLine();
+        System.out.println("What is the name of the location?");
+        String name = scanner.nextLine();
+        String insert = String.format("INSERT INTO locations VALUES (null, '%s' , '%s' , '%s')"
+                , city, address, name);
+
+        statement.executeUpdate(insert);
+        statement.close();
     }
 
 }
